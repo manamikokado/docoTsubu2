f3648e580543e5c1d784206c3dc051a0ab553da4
mnmkkd <chine.mybestway.kim313@gmail.com>
2020-12-19 23:37:36 +0900

    main.jspを作成

---
+++ src/servlet/Main.java
@@ -0,0 +1,71 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Tweet;
+import model.User;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// アプリケーションスコープからつぶやきリストを取得
+		ServletContext application = this.getServletContext();
+		List<Tweet> tweetList = (List<Tweet>) application.getAttribute("tweetList");
+		// 取得できなかった場合はつぶやきリストを新規作成して
+		// アプリケーションスコープに保存
+		if(tweetList == null) {
+			tweetList = new ArrayList<>();
+			application.setAttribute("tweetList", tweetList);
+		}
+
+		// ログインしているか確認するため
+		// セッションスコープからユーザ情報を取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		if(loginUser == null) {
+			// ログインしていない場合はリダイレクト
+			response.sendRedirect("/docoTsubu/");
+		} else {
+			// ログインしている場合はフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
